a310a8f8e4192e19ab762a90030a70e0b76f8894
Adenuga <adenuga.ap@gmail.com>
2025-08-16 22:44:16 +0200

    udater

--- Adenuga_task6/task_1.py
+++ Adenuga_task6/task_1.py
@@ -1,20 +1,20 @@
 # fruits collector
 # 1
-# User_fav= set()
-# # User Inpu7
-# fruits1 = input("Enter your favorite fruit: ")
-# fruits2 = input("Enter your favorite fruit: ")
-# fruits3 = input("Enter your favorite fruit: ")
-# fruits4 = input("Enter your favorite fruit: ")
-# fruits5 = input("Enter your favorite fruit: ")
-# # User_fav = set()
-# User_fav.add(fruits1)
-# User_fav.add(fruits2)
-# User_fav.add(fruits3)
-# User_fav.add(fruits4)
-# User_fav.add(fruits5)
+User_fav= set()
+# User Inpu7
+fruits1 = input("Enter your favorite fruit: ")
+fruits2 = input("Enter your favorite fruit: ")
+fruits3 = input("Enter your favorite fruit: ")
+fruits4 = input("Enter your favorite fruit: ")
+fruits5 = input("Enter your favorite fruit: ")
+# User_fav = set()
+User_fav.add(fruits1)
+User_fav.add(fruits2)
+User_fav.add(fruits3)
+User_fav.add(fruits4)
+User_fav.add(fruits5)
+print(User_fav)
+# Another approach
+# fruits1 = input("Enter your five favorite fruit with spaces: ")
+# User_fav = set(fruits1.split())
 # print(User_fav)
-# # Another approach
-# # fruits1 = input("Enter your five favorite fruit with spaces: ")
-# # User_fav = set(fruits1.split())
-# # print(User_fav)
